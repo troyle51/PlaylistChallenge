8dbf227377ddf8e4ca4e6c8a2c005a21c8ce7c36
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-07-06 13:32:43 -0400

    checkpoint

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,23 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int index = 0;
+        int temp = startIndex;
+        int clicks = 0;
+
+        for(int i = 0; i < this.playList.length; i++){
+            if(this.playList[i].equals(selection)){
+                index = i;
+                }
+            }
+        while(temp < index){
+            clicks++;
+            temp++;
+        }
+
+        if(index == this.playList.length -1){
+            return 1;
+        }
+        return clicks;
     }
 }
